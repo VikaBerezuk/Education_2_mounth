5c498f39f8f1c4aef59d6028ced304ed888b12cf
viktoria <vikaberezuk12345@gmail.com>
2022-02-12 22:44:51 +0200

    application painter canvas test

--- 11_Canvas/11_AppPainter/src/tests/Painter.test.js
+++ 11_Canvas/11_AppPainter/src/tests/Painter.test.js
@@ -2,13 +2,24 @@ const {Painter} = require('./../Painter');
 
 describe('Painter', () => {
     const painter = new Painter({strokeStyle:'#000000', beginPath(){}, moveTo(){}, lineTo(){}, stroke(){}});
-    test('Painter start', () => {
+    test('should be defined', () => {
+        expect(painter.start).toBeDefined();
+    })
+    test('Painter start, should be undefined', () => {
         expect(painter.start()).toBeUndefined();
+    })
+
+    test('should be defined', () => {
+        expect(painter.stop).toBeDefined();
     })
     test('Painter start', () => {
         expect(painter.stop()).toBeUndefined();
     })
-    test('Painter line', () => {
+
+    test('should be defined', () => {
+        expect(painter.line).toBeDefined();
+    })
+    test('Painter line, should be undefined', () => {
         expect(painter.line()).toBeUndefined();
     })
 })
